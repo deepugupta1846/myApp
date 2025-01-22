d29be7299046cf5d45ee7c969084ae3c77b60903
deepukumar4626 <deepu.kumar@avidialabs.com>
2025-01-22 08:33:26 +0530

    2.0

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mb91xwz--1mxa84+gzyeudbw0s=r+$fn_bdv7_xx_o@o9z)@%*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
