a9baeac867f043a3efefc4d7f3dcc4602ddc74e7
deepukumar4626 <deepu.kumar@avidialabs.com>
2025-01-22 17:51:13 +0530

    16.0

--- myapp/settings.py
+++ myapp/settings.py
@@ -93,14 +93,18 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'HOST': 'mysql.railway.internal',
-        'PORT': '3306',
-        'PASSWORD': 'ftxcSwRTncTMDVwCyJJoYmppqsZkeBmQ',
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'railway',
+    #     'USER': 'root',
+    #     'HOST': 'mysql.railway.internal',
+    #     'PORT': '3306',
+    #     'PASSWORD': 'ftxcSwRTncTMDVwCyJJoYmppqsZkeBmQ',
+    # },
 }
 
 
